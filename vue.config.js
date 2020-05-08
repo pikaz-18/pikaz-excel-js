a75da8be820abc426a2c9b9162cbf88214c24a8f
zouzheng <qq2363994579@outlook.com>
2020-05-08 14:08:34 +0800

    修改打包路径为相对路径

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: zouzheng
+ * @Date: 2020-05-08 14:06:58
+ * @LastEditors: zouzheng
+ * @LastEditTime: 2020-05-08 14:07:47
+ * @Description: 这是XXX组件（页面）
+ */
+module.exports = {
+  publicPath: "./"
+};
